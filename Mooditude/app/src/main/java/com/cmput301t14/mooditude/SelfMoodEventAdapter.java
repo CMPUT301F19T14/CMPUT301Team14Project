37314881fd6cdea0b5ebda7a5bf0aafa082463e8
gza060625 <gza060625@gmail.com>
2019-11-08 16:54:49 -0700

    change time format in listView

--- Mooditude/app/src/main/java/com/cmput301t14/mooditude/SelfMoodEventAdapter.java
+++ Mooditude/app/src/main/java/com/cmput301t14/mooditude/SelfMoodEventAdapter.java
@@ -1,6 +1,7 @@
 package com.cmput301t14.mooditude;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 
 /**
@@ -20,6 +23,7 @@ import java.util.ArrayList;
 public class SelfMoodEventAdapter extends ArrayAdapter<MoodEvent> {
     private ArrayList<MoodEvent> moodEventList;
     private Context context;
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
     /**
      * Constructor for the adapter
@@ -45,6 +49,7 @@ public class SelfMoodEventAdapter extends ArrayAdapter<MoodEvent> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View view = convertView;
 
+
         if(view == null){
             view = LayoutInflater.from(context).inflate(R.layout.self_mood_event_list_content, parent,false);
         }
@@ -54,7 +59,10 @@ public class SelfMoodEventAdapter extends ArrayAdapter<MoodEvent> {
         TextView timeTextView = view.findViewById(R.id.time_textview);
         TextView emoticonTextView = view.findViewById(R.id.emoticon_textview);
 
-        timeTextView.setText(moodEvent.getDatetime().toString());
+
+//        String string= ;
+        timeTextView.setText(sdf.format(new Date(moodEvent.getDatetime().getSeconds()*1000)));
+
         emoticonTextView.setText(moodEvent.getMood().getEmoticon());
         LinearLayout moodEventEntry= view.findViewById(R.id.mood_event_entry);
         moodEventEntry.setBackgroundColor(moodEvent.getMood().getColor());
