23b1ff204cc947a4afd98c59c96e2ff4790a76dd
gza060625 <gza060625@gmail.com>
2019-11-10 23:04:58 -0700

    try to close key board in search

--- Mooditude/app/src/main/java/com/cmput301t14/mooditude/SearchActivity.java
+++ Mooditude/app/src/main/java/com/cmput301t14/mooditude/SearchActivity.java
@@ -1,8 +1,12 @@
 package com.cmput301t14.mooditude;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
