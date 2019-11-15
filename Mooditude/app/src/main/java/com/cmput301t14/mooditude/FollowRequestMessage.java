d1171446e3c73ae681b199709e9f2cea1c8867d4
Liyao Jiang <liyao1@ualberta.ca>
2019-11-15 12:17:53 -0700

    adding accept and reject follow message backend

--- Mooditude/app/src/main/java/com/cmput301t14/mooditude/FollowRequestMessage.java
+++ Mooditude/app/src/main/java/com/cmput301t14/mooditude/FollowRequestMessage.java
@@ -5,8 +5,9 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.util.Date;
 import java.util.HashMap;
+
+import static java.lang.Boolean.TRUE;
 
 public class FollowRequestMessage extends Message {
     public FollowRequestMessage(String receiver) {
@@ -35,17 +36,11 @@ public class FollowRequestMessage extends Message {
     }
 
     @Override
-    String toStringContent() {
+    public String toStringContent() {
         return this.sender+" wants to follow you";
     }
 
-    @Override
-    String toStringDatetime() {
-        return super.sdf.format(new Date(this.datetime.getSeconds() * 1000));
-    }
-
-    @Override
-    void invoke() {
+    public void invoke() {
         CollectionReference usersCollection = FirebaseFirestore.getInstance().collection("Users");
         CollectionReference receiverMsgBox= usersCollection.document(receiver).collection("MessageBox");
         String epochTimeString= String.valueOf(this.datetime.getSeconds());
@@ -57,5 +52,52 @@ public class FollowRequestMessage extends Message {
         messageHash.put("datetime",this.datetime);
         messageHash.put("type",this.type);
         messageEntry.set(messageHash);
+    }
+
+    public void accept(){
+        CollectionReference usersCollection = FirebaseFirestore.getInstance().collection("Users");
+
+        // add sender to receiver's Followers collection
+        CollectionReference receiverFollowers = usersCollection.document(this.receiver).collection("Followers");
+        final DocumentReference receiverFollowersEntry= receiverFollowers.document(this.sender);
+        final HashMap<String,Object> followerHash = new HashMap<>();
+        receiverFollowersEntry.set(followerHash);
+
+        // add receiver to sender's Followings collection
+        CollectionReference senderFollowings = usersCollection.document(this.sender).collection("Followings");
+        final DocumentReference senderFollowingsEntry= senderFollowings.document(this.receiver);
+        final HashMap<String,Object> followingHash = new HashMap<>();
+        // TODO: get receiver's most recent MoodEvent and put it in the senderFollowingsEntry
+//        followerHash.put("type", "reject");
+        senderFollowingsEntry.set(followingHash);
+
+        // send accept message to sender's MessageBox collection
+        CollectionReference senderMsgBox= usersCollection.document(this.sender).collection("MessageBox");
+        String epochTimeString= String.valueOf(Timestamp.now().getSeconds());
+        final DocumentReference messageEntry=senderMsgBox.document(epochTimeString);
+        final HashMap<String,Object> messageHash = new HashMap<>();
+        messageHash.put("sender",this.receiver);
+        messageHash.put("receiver",this.sender);
+        messageHash.put("newMessage", TRUE);
+        messageHash.put("datetime", Timestamp.now());
+        messageHash.put("type", "accept");
+        messageEntry.set(messageHash);
+        // TODO: change the type of this message to completed
+    }
+
+    public void reject(){
+        // send reject message to sender's MessageBox collection
+        CollectionReference usersCollection = FirebaseFirestore.getInstance().collection("Users");
+        CollectionReference senderMsgBox= usersCollection.document(this.sender).collection("MessageBox");
+        String epochTimeString= String.valueOf(Timestamp.now().getSeconds());
+        final DocumentReference messageEntry=senderMsgBox.document(epochTimeString);
+        final HashMap<String,Object> messageHash = new HashMap<>();
+        messageHash.put("sender",this.receiver);
+        messageHash.put("receiver",this.sender);
+        messageHash.put("newMessage", TRUE);
+        messageHash.put("datetime", Timestamp.now());
+        messageHash.put("type", "reject");
+        messageEntry.set(messageHash);
+        // TODO: change the type of this message to completed
     }
 }
--- Mooditude/app/src/main/java/com/cmput301t14/mooditude/Message.java
+++ Mooditude/app/src/main/java/com/cmput301t14/mooditude/Message.java
@@ -4,6 +4,7 @@ import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.CollectionReference;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import static java.lang.Boolean.TRUE;
 
@@ -50,7 +51,11 @@ public abstract class Message {
         this.newMessage=TRUE;
     }
 
-    abstract void invoke();
-    abstract String toStringContent();
-    abstract String toStringDatetime();
+    public abstract String toStringContent();
+
+    public String toStringDatetime() {
+        return sdf.format(new Date(this.datetime.getSeconds() * 1000));
+    }
+
+    // TODO: Implement delete() method
 }
