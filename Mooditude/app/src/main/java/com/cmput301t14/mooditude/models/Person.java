2bcec6d57ed1c7afc649a6a1a997dacf8b65b40f
gza060625 <gza060625@gmail.com>
2019-11-28 16:05:50 -0700

    comment

--- Mooditude/app/src/main/java/com/cmput301t14/mooditude/models/Person.java
+++ Mooditude/app/src/main/java/com/cmput301t14/mooditude/models/Person.java
@@ -1,26 +1,51 @@
 package com.cmput301t14.mooditude.models;
 
+
+/**
+ * Define one user class contain name and email to identify one person
+ */
 public class Person {
     private String name;
     private String email;
 
+    /**
+     * Constructor
+     * @param email
+     * @param name
+     */
     public Person( String email, String name) {
         this.name = name;
         this.email = email;
     }
 
+    /**
+     * get name
+     * @return
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * set name
+     * @param name
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * get email
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * set email
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
