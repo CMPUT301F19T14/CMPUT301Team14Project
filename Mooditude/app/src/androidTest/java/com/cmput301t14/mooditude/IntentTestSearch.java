803e333dbd8edfaac7a9196eb482065b61b83308
Michael Xing <xiaojie1@ualberta.ca>
2019-11-28 21:35:42 -0700

    created various unitTests

--- Mooditude/app/src/androidTest/java/com/cmput301t14/mooditude/IntentTestSearch.java
+++ Mooditude/app/src/androidTest/java/com/cmput301t14/mooditude/IntentTestSearch.java
@@ -1,5 +1,64 @@
 package com.cmput301t14.mooditude;
 
-public class IntentTestXianda {
+import android.app.Activity;
+import android.widget.EditText;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+
+import com.cmput301t14.mooditude.activities.HomeActivity;
+import com.cmput301t14.mooditude.activities.MainActivity;
+import com.cmput301t14.mooditude.activities.SearchActivity;
+import com.cmput301t14.mooditude.activities.SelfActivity;
+import com.cmput301t14.mooditude.activities.SignInActivity;
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class IntentTestSearch {
+
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, true);
+
+
+    @Before
+    public void setUp() throws Exception {
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
+
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+    }
+
+    @Test
+    public void start() throws Exception {
+        Activity activity = rule.getActivity();
+    }
+
+    @Test
+    public void testSearchUser(){
+        solo.clickOnButton("Sign In");
+        solo.waitForActivity(SignInActivity.class);
+        solo.assertCurrentActivity("Wrong Activity", SignInActivity.class);
+
+
+        String email = "ui2@test.com";
+        solo.enterText((EditText) solo.getView(R.id.signin_email_edit_text), email);
+        solo.enterText((EditText) solo.getView(R.id.signin_password_edit_text), "123456");
+
+        solo.clickOnButton("Sign In");
+        solo.waitForActivity(HomeActivity.class);
+
+        solo.assertCurrentActivity("Wrong Activity", HomeActivity.class);
+
+        solo.clickOnView(solo.getView(R.id.navigation_search));
+        solo.waitForActivity(SearchActivity.class);
+        solo.assertCurrentActivity("Wrong Activity", SearchActivity.class);
+
+        solo.enterText((EditText) solo.getView(R.id.search_edit_text), "ui@test.com");
+    }
 
 }
--- Mooditude/app/src/test/java/com/cmput301t14/mooditude/PersonUnitTest.java
+++ Mooditude/app/src/test/java/com/cmput301t14/mooditude/PersonUnitTest.java
@@ -1,4 +1,22 @@
 package com.cmput301t14.mooditude;
 
+import com.cmput301t14.mooditude.models.Person;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class PersonUnitTest {
+
+    @Test
+    public void personClassTest(){
+        Person person = new Person("test@test.com","test");
+        Assert.assertTrue(person.getEmail().equals("test@test.com"));
+        Assert.assertTrue(person.getName().equals("test"));
+
+        person.setEmail("set@set.com");
+        person.setName("set");
+        Assert.assertTrue(person.getEmail().equals("set@set.com"));
+        Assert.assertTrue(person.getName().equals("set"));
+
+    }
 }
